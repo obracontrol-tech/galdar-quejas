131202874cd9703cf7126c4f935e545381f70c89
obracontrol-tech <pedrosaborrego@gmail.com>
2026-04-28 00:19:07 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,28 +1,21 @@
-const CACHE = 'galdar-quejas-v1';
-const ASSETS = [
-  './',
-  './index.html',
-  './style.css',
-  './manifest.json',
-  './logo.png'
-];
+const CACHE = 'galdar-vecinos-v5';
+const ASSETS = ['./', './index.html', './style.css', './manifest.json', './logo.png'];
 
 self.addEventListener('install', e => {
-  e.waitUntil(
-    caches.open(CACHE).then(cache => cache.addAll(ASSETS))
-  );
-});
-
-self.addEventListener('fetch', e => {
-  e.respondWith(
-    caches.match(e.request).then(cached => cached || fetch(e.request))
-  );
+  self.skipWaiting();
+  e.waitUntil(caches.open(CACHE).then(cache => cache.addAll(ASSETS)));
 });
 
 self.addEventListener('activate', e => {
   e.waitUntil(
     caches.keys().then(keys =>
       Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
-    )
+    ).then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    fetch(e.request).catch(() => caches.match(e.request))
   );
 });
